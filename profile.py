f8e7e365714a915ab43baaec1e4aa25a5e8a60c7
JarrettTrapani <89924103+JarrettTrapani@users.noreply.github.com>
2024-02-07 16:37:43 -0500

    Update profile.py RawPC

--- profile.py
+++ profile.py
@@ -4,8 +4,8 @@ import geni.rspec.pg as rspec
 # Create a Request object to start building the RSpec.
 request = portal.context.makeRequestRSpec()
 # Create a XenVM
-node = request.XenVM("node")
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
+node = request.RawPC("node")
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
 node.routable_control_ip = "true"
 
 node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt update"))
